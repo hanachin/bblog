42dbe2946f141e70f36ef156e80094a732f83e4d
Seiei Miyagi <hanachin@gmail.com>
2018-01-06 08:38:35 +0900

    render body by hyperapp

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import '../styles/application.sass'
+import { h, app} from 'hyperapp'
+
+const view = (state, actions) => (
+  <main>
+    <h1>👶🤱🍼💩 </h1>
+  </main>
+)
+
+app({}, {}, view, document.body)
