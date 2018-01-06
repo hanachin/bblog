22bfbf624e00ce1ae9fecde3895b58913e7c0d85
Seiei Miyagi <hanachin@gmail.com>
2018-01-06 23:08:19 +0900

    Fix CurrentTime component input value format

--- app/javascript/baby-logs/components/CurrentTime.js
+++ app/javascript/baby-logs/components/CurrentTime.js
@@ -1,6 +1,11 @@
 import "../styles/CurrentTime";
 import { h } from "hyperapp";
 
-export const CurrentTime = () => (
-  <input className="current-time" type="time" value={`${new Date().getHours()}:${new Date().getMinutes()}`}/>
-)
+export const CurrentTime = () => {
+  const date = new Date();
+  const hours = `${date.getHours()}`.padStart(2, '0');
+  const minutes = `${date.getMinutes()}`.padStart(2, '0');
+  return (
+    <input className="current-time" type="time" value={`${hours}:${minutes}`}/>
+  );
+}
