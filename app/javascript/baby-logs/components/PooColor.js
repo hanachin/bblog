42b18959801a8e85dee040ebee5a60d0639f05f5
Seiei Miyagi <hanachin@gmail.com>
2018-01-07 22:59:38 +0900

    Update PooColor style

--- app/javascript/baby-logs/components/PooColor.js
+++ app/javascript/baby-logs/components/PooColor.js
@@ -2,7 +2,7 @@ import "../styles/PooColor";
 import { h } from "hyperapp";
 
 export const PooColor = () => (
-  <div>
-    {Array.from(Array(7).keys()).map(n => <div className={`poo-color poo-color--${n+1}`}>{n+1}</div>)}
+  <div className="poo-color">
+    {Array.from(Array(7).keys()).map(n => <div>{n+1}</div>)}
   </div>
 )
