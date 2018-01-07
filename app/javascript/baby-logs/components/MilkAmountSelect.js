3c78f1f97dc6d94cff2cea922e13bbf4664fa0b6
Seiei Miyagi <hanachin@gmail.com>
2018-01-07 21:15:54 +0900

    Remove unnecessary blank space

--- app/javascript/baby-logs/components/MilkAmountSelect.js
+++ app/javascript/baby-logs/components/MilkAmountSelect.js
@@ -3,6 +3,6 @@ import { h } from "hyperapp";
 
 export const MilkAmountSelect = () => (
   <select className="milk-amount-select">
-    {Array.from(Array(100).keys()).map((i) => <option value={(i + 1) * 10}>{(i + 1) * 10} ml</option>)}
+    {Array.from(Array(100).keys()).map((i) => <option value={(i + 1) * 10}>{(i + 1) * 10}ml</option>)}
   </select>
 )
