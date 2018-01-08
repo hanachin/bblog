f996e54cc174f400e5e4bdb50f293dafa3a92b98
Seiei Miyagi <hanachin@gmail.com>
2018-01-08 15:04:22 +0900

    Make PooColor component to real input

--- app/javascript/baby-logs/components/PooColor.js
+++ app/javascript/baby-logs/components/PooColor.js
@@ -3,6 +3,6 @@ import { h } from "hyperapp";
 
 export const PooColor = () => (
   <div className="poo-color">
-    {Array.from(Array(7).keys()).map(n => <div>{n+1}</div>)}
+    {Array.from(Array(7).keys()).map(n => <input name="poo-color" type="radio" value={n+1}/>)}
   </div>
 )
