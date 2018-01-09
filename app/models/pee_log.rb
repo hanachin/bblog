171cb2f894bf2328cd009c7395023f02d1b2489e
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 03:21:50 +0900

    Add PeeLog.baby_logs_sql

--- app/models/pee_log.rb
+++ app/models/pee_log.rb
@@ -1,3 +1,17 @@
 class PeeLog < ApplicationRecord
   include DiaperUsage
+
+  class << self
+    def baby_logs_sql
+      <<~SQL
+      (
+        select
+          '#{model_name.human}' as type,
+          (#{table_name}.started_at at time zone 'UTC' at time zone '#{Time.zone.name}') as started_at,
+          (#{diaper_usage_text_sql}) as text
+        from #{table_name}
+      )
+    SQL
+    end
+  end
 end
--- spec/models/pee_log_spec.rb
+++ spec/models/pee_log_spec.rb
@@ -4,6 +4,18 @@ RSpec.describe PeeLog, type: :model do
   it_behaves_like 'have .model_name.human', '💧'
   it_behaves_like 'have diaper_usage enum'
 
+  describe '.baby_logs_sql' do
+    before do
+      started_at = Time.zone.local(2017, 1, 2, 3, 4, 5)
+      PeeLog.create!(diaper_usage: :much, started_at: started_at)
+    end
+
+    specify do
+      actual = ApplicationRecord.connection.exec_query(PeeLog.baby_logs_sql).to_a
+      expect(actual).to eq([{ "type" => "💧", "started_at" => "2017-01-02 03:04:05", "text" => "💧💧💧" }])
+    end
+  end
+
   describe '.diaper_usage_text_sql' do
     before do
       PeeLog.create!(started_at: Time.current, diaper_usage: :no_input)
