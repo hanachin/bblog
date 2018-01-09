fb3f60f15a7981f292dbfa4fe756628e021359e1
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 02:04:35 +0900

    Add BreastMilkLog.side_text_sql

--- app/models/breast_milk_log.rb
+++ app/models/breast_milk_log.rb
@@ -1,3 +1,17 @@
 class BreastMilkLog < ApplicationRecord
   enum side: %i(no_input left right), _suffix: :side
+
+  class << self
+    def side_text_sql
+      <<~SQL
+      (
+        CASE side
+        WHEN #{sides.fetch(:left)} THEN '#{human_attribute_name(:left_side)}'
+        WHEN #{sides.fetch(:right)} THEN '#{human_attribute_name(:right_side)}'
+        ELSE NULL
+        END
+      )
+      SQL
+    end
+  end
 end
--- spec/models/breast_milk_log_spec.rb
+++ spec/models/breast_milk_log_spec.rb
@@ -3,4 +3,18 @@ require 'rails_helper'
 RSpec.describe BreastMilkLog, type: :model do
   it_behaves_like 'have .model_name.human', '🤱'
   it { is_expected.to define_enum_for(:side).with(%i(no_input left right)) }
+
+  describe '.side_text_sql' do
+    before do
+      BreastMilkLog.create!(duration_min: 1, started_at: Time.current, side: :no_input)
+      BreastMilkLog.create!(duration_min: 1, started_at: Time.current, side: :left)
+      BreastMilkLog.create!(duration_min: 1, started_at: Time.current, side: :right)
+    end
+
+    specify do
+      logs = BreastMilkLog.select(:id, :side, "#{BreastMilkLog.side_text_sql} as side_text").order(:id)
+      actual = logs.inject([]) { |rs, r| rs << [r[:side], r[:side_text]] }
+      expect(actual).to eq([["no_input", nil], %w(left 左), %w(right 右)])
+    end
+  end
 end
