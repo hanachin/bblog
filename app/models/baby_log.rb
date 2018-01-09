09896005d874686eef909c9d51e40ab8fa3cfb85
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 03:58:06 +0900

    Add BabyLog.{baby_logs_sql,to_json}

--- app/models/baby_log.rb
+++ app/models/baby_log.rb
@@ -1,4 +1,31 @@
 class BabyLog
   extend ActiveModel::Naming
   extend ActiveModel::Translation
+
+  class << self
+    def baby_logs_sql
+      <<~SQL
+      (
+        select
+          (baby_logs.type || ' ' || to_char(baby_logs.started_at, 'HH24:MI') || coalesce((' ' || baby_logs.text), '')) as text, baby_logs.started_at
+        from (
+          (#{BathLog.baby_logs_sql})
+          union (#{BreastMilkLog.baby_logs_sql})
+          union (#{MilkLog.baby_logs_sql})
+          union (#{PeeLog.baby_logs_sql})
+          union (#{PooLog.baby_logs_sql})
+        ) as baby_logs
+      )
+      SQL
+    end
+
+    def to_json
+      ApplicationRecord.connection.select_value(<<~SQL)
+      select
+        json_build_object(started_at::date, array_to_json(array_agg(text ORDER BY started_at desc)))
+      from #{baby_logs_sql} as baby_logs
+      group by started_at::date;
+      SQL
+    end
+  end
 end
--- spec/models/baby_log_spec.rb
+++ spec/models/baby_log_spec.rb
@@ -2,4 +2,43 @@ require 'rails_helper'
 
 RSpec.describe BabyLog do
   it_behaves_like 'have .model_name.human', '👶'
+
+  shared_context 'there are some baby logs' do
+    before do
+      now = Time.zone.local(2017, 1, 2, 3, 4, 5)
+      BathLog.create!(duration_min: 1, started_at: now)
+      BreastMilkLog.create!(duration_min: 2, started_at: 1.hour.since(now))
+      MilkLog.create!(duration_min: 3, milk_volume_ml: 120, started_at: 2.hours.since(now))
+      PeeLog.create!(started_at: 3.hours.since(now))
+      PooLog.create!(started_at: 4.hours.since(now))
+    end
+  end
+
+  describe '.baby_logs_sql' do
+    include_context 'there are some baby logs'
+
+    specify do
+      result = ApplicationRecord.connection.exec_query("select * from (#{BabyLog.baby_logs_sql}) as t order by t.started_at desc").to_a
+      actual = result.map { |r| r['text'] }
+      expect(actual).to eq(["💩 07:04", "💧 06:04", "🍼 05:04 3分 120ml", "🤱 04:04 2分", "🛀 03:04 1分"])
+    end
+  end
+
+  describe '.to_json' do
+    include_context 'there are some baby logs'
+
+    specify do
+      expected = {
+        "2017-01-02" => [
+          "💩 07:04",
+          "💧 06:04",
+          "🍼 05:04 3分 120ml",
+          "🤱 04:04 2分",
+          "🛀 03:04 1分"
+        ]
+      }
+      actual = JSON.parse(BabyLog.to_json)
+      expect(actual).to eq(expected)
+    end
+  end
 end
