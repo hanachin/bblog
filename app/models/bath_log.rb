34a5d390604df070645157cd7995db263465a04b
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 02:51:07 +0900

    Add BathLog.baby_logs_sql

--- app/models/bath_log.rb
+++ app/models/bath_log.rb
@@ -1,2 +1,15 @@
 class BathLog < ApplicationRecord
+  class << self
+    def baby_logs_sql
+      <<~SQL
+      (
+        select
+          '#{model_name.human}' as type,
+          (#{table_name}.started_at at time zone 'UTC' at time zone '#{Time.zone.name}') as started_at,
+          (#{table_name}.duration_min || '#{human_attribute_name(:duration_min)}') as text
+        from #{table_name}
+      )
+      SQL
+    end
+  end
 end
--- spec/models/bath_log_spec.rb
+++ spec/models/bath_log_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe BathLog, type: :model do
   it_behaves_like 'have .model_name.human', '🛀'
+
+  describe '.baby_logs_sql' do
+    before do
+      started_at = Time.zone.local(2017, 1, 2, 3, 4, 5)
+      BathLog.create!(duration_min: 1, started_at: started_at)
+    end
+
+    specify do
+      actual = ApplicationRecord.connection.exec_query(BathLog.baby_logs_sql).to_a
+      expect(actual).to eq([{ "type" => "🛀", "started_at" => "2017-01-02 03:04:05", "text" => "1分" }])
+    end
+  end
 end
