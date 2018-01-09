fc638c67e21822f59c942223fde4f817f65a4473
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 03:16:08 +0900

    Add MilkLog.baby_logs_sql

--- app/models/milk_log.rb
+++ app/models/milk_log.rb
@@ -1,2 +1,15 @@
 class MilkLog < ApplicationRecord
+  class << self
+    def baby_logs_sql
+      <<~SQL
+      (
+        select
+          '#{model_name.human}' as type,
+          (#{table_name}.started_at at time zone 'UTC' at time zone '#{Time.zone.name}') as started_at,
+          (#{table_name}.duration_min || '#{human_attribute_name(:duration_min)}' || ' ' || #{table_name}.milk_volume_ml || '#{human_attribute_name(:milk_volume_ml)}') as text
+        from #{table_name}
+      )
+      SQL
+    end
+  end
 end
--- spec/models/milk_log_spec.rb
+++ spec/models/milk_log_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe MilkLog, type: :model do
   it_behaves_like 'have .model_name.human', '🍼'
+
+  describe '.baby_logs_sql' do
+    before do
+      started_at = Time.zone.local(2017, 1, 2, 3, 4, 5)
+      MilkLog.create!(duration_min: 1, milk_volume_ml: 100, started_at: started_at)
+    end
+
+    specify do
+      actual = ApplicationRecord.connection.exec_query(MilkLog.baby_logs_sql).to_a
+      expect(actual).to eq([{ "type" => "🍼", "started_at" => "2017-01-02 03:04:05", "text" => "1分 100ml" }])
+    end
+  end
 end
