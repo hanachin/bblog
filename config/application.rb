a20873ce0fad1ffaa13de0db8686721a9eaed9e8
Seiei Miyagi <hanachin@gmail.com>
2018-01-10 09:59:10 +0900

    Disable assets/helper/template_engine generators

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ module Bblog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators.assets = nil
+    config.generators.helper = nil
+    config.generators.template_engine = nil
     # Don't generate system test files.
     config.generators.system_tests = nil
 
