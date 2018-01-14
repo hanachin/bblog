db204982e4116e5000fddd718c9649b9f31d604b
Seiei Miyagi <hanachin@gmail.com>
2018-01-14 14:50:10 +0900

    Update baby_logs#index routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post 'verification_emails', to: 'verification_emails#create'
 
   constraints(year: /\d{4}/, month: /\d{1,2}/, mday: /\d{1,2}/) do
-    get 'baby/:year/:month/:mday', to: 'baby_logs#index'
+    get 'baby/:year/:month/:mday', to: 'baby_logs#index', as: :baby_logs
     post 'baby/bath_logs', to: 'bath_logs#create'
     post 'baby/breast_milk_logs', to: 'breast_milk_logs#create'
     post 'baby/milk_logs', to: 'milk_logs#create'
