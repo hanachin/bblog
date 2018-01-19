e1b85a1ec360a484272adb0e65ad20ae36865ce1
Seiei Miyagi <hanachin@gmail.com>
2018-01-20 00:03:43 +0900

    Fix baby log order

--- app/javascript/components/Logs.js
+++ app/javascript/components/Logs.js
@@ -3,9 +3,12 @@ import { h } from "hyperapp";
 
 export const Logs = ({ state: { logs }, actions }) => (
   <div>
-    {Object.keys(logs).map(d => [
-      <h1>{d}</h1>,
-      <ul className="logs">{logs[d].map(log => <li>{log}</li>)}</ul>
-    ])}
+    {Object.keys(logs)
+      .sort()
+      .reverse()
+      .map(d => [
+        <h1>{d}</h1>,
+        <ul className="logs">{logs[d].map(log => <li>{log}</li>)}</ul>
+      ])}
   </div>
 );
