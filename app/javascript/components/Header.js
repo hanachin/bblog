4e3aed1a623b5b44827a932d28837c69964872b0
Seiei Miyagi <hanachin@gmail.com>
2018-01-20 00:12:14 +0900

    Fetch latest baby logs when click the baby icon

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,8 +1,9 @@
 import "styles/components/Header";
 import { h } from "hyperapp";
 import { LogTypes } from "constants/LogTypes";
+import { getBabyLogs } from "requests/getBabyLogs";
 
-export const Header = ({ state, actions }) => (
+export const Header = ({ state, actions: { changeLogType, loadLogs } }) => (
   <header>
     <nav>
       <ul className="log-types">
@@ -10,7 +11,16 @@ export const Header = ({ state, actions }) => (
           <li
             className={`log-types__log-type ${t == state.logType &&
               "log-types__log-type--current"}`}
-            onclick={() => actions.changeLogType(t)}
+            onclick={() => {
+              changeLogType(t);
+              if (t !== "👶") {
+                return;
+              }
+              (async () => {
+                const logs = await getBabyLogs(new Date());
+                loadLogs(logs);
+              })();
+            }}
           >
             <span>{t}</span>
           </li>
